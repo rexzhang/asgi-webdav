a73efcbb2e1db86fc52990ab5eee0cae516cba47
Rex Zhang <rex.zhang@gmail.com>
2021-08-04 20:15:36 +0800

    Fix bug #3

--- br_wheel.sh
+++ br_wheel.sh
@@ -4,6 +4,7 @@ pip install -U wheel pip
 
 rm -rf build dist
 python setup.py sdist bdist_wheel
+wheel_package=`ls dist/*.whl`
 
-pip uninstall -y ASGI-WebDAV
-pip install dist/ASGI_WebDAV-0.7.0-py2.py3-none-any.whl
+pip uninstall -y ASGI-WebDAV-Server
+pip install $wheel_package
