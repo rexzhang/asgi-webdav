409a0e4cbbecc8991e3637afe2941856a0b68232
Rex Zhang <rex.zhang@gmail.com>
2022-08-22 17:32:06 +0800

    Update, pep517/pep621; LDAP is now optional

--- publish_to_pypi.sh
+++ publish_to_pypi.sh
@@ -4,6 +4,6 @@ python -m pip install -U -r requirements/pypi.txt
 
 rm -rf build/*
 rm -rf dist/*
-python setup.py sdist bdist_wheel
+python -m build
 
 python -m twine upload --repository-url https://upload.pypi.org/legacy/ dist/*
