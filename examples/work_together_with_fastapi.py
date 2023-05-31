4d97a3aebb7fd1c3607dfe34de93259435b38f25
David Brochart <david.brochart@gmail.com>
2023-05-31 09:52:16 +0200

    Lint

--- examples/work_together_with_fastapi.py
+++ examples/work_together_with_fastapi.py
@@ -16,16 +16,20 @@ webdav_config = {
 webdav_aep = AppEntryParameters()
 webdav_app = get_webdav_asgi_app(aep=webdav_aep, config_obj=webdav_config)
 
+
 class WebDAVApp:
     def __init__(self, app, webdav_app):
         self._app = app
         self._webdav_app = webdav_app
 
     async def __call__(self, scope, receive, send):
-        if scope.get("method") in DAV_METHODS and scope.get("path").startswith("/webdav"):
+        if scope.get("method") in DAV_METHODS and scope.get("path").startswith(
+            "/webdav"
+        ):
             return await self._webdav_app(scope, receive, send)
 
         return await self._app(scope, receive, send)
+
 
 app = FastAPI()
 app.add_middleware(partial(WebDAVApp, webdav_app=webdav_app))
