b1b5e336609d7cb0c54ff369dea51521b5586a09
Rex Zhang <rex.zhang@gmail.com>
2023-06-10 11:41:42 +0800

    Add black hook of pre-commit

--- asgi_webdav/helpers.py
+++ asgi_webdav/helpers.py
@@ -67,9 +67,7 @@ def generate_etag(f_size: [float, int], f_modify_time: float) -> str:
     https://tools.ietf.org/html/rfc7232#section-2.3 ETag
     https://developer.mozilla.org/zh-CN/docs/Web/HTTP/Headers/ETag
     """
-    return 'W/"{}"'.format(
-        hashlib.md5(f"{f_size}{f_modify_time}".encode()).hexdigest()
-    )
+    return 'W/"{}"'.format(hashlib.md5(f"{f_size}{f_modify_time}".encode()).hexdigest())
 
 
 def guess_type(config: Config, file: str | Path) -> (str | None, str | None):
