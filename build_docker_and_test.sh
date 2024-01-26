a22572e66fe8fc4cd34218b04d1b6d5cbf3de011
Rex Zhang <rex.zhang@gmail.com>
2024-01-26 17:14:07 +0800

    Update docker base image

--- build_docker_and_test.sh
+++ build_docker_and_test.sh
@@ -1,17 +1,21 @@
+#!/bin/zsh
+
+docker pull python:3.12-alpine
+docker build -t cr.h.rexzhang.com/ray1ex/asgi-webdav . --build-arg ENV=rex
+
+read -r -s -k '?Press any key to continue, push docker image...'
+docker push cr.h.rexzhang.com/ray1ex/asgi-webdav
+
+read -r -s -k '?Press any key to continue. startup container...'
 docker container stop asgi-webdav
 docker container rm asgi-webdav
-docker image rm ray1ex/asgi-webdav
-
-docker pull python:3.10-alpine
-docker build -t ray1ex/asgi-webdav . --build-arg ENV=dev
-
 mkdir /tmp/webdav
 docker run -dit --restart unless-stopped \
   -p 0.0.0.0:8000:8000 \
   -v /tmp/webdav:/data \
   -e UID=501 -e GID=20 \
-  -e WEBDAV_ENV=dev \
+  -e DEBUG=true \
   -e WEBDAV_LOGGING_LEVEL=DEBUG \
-  --name asgi-webdav ray1ex/asgi-webdav
+  --name asgi-webdav cr.h.rexzhang.com/ray1ex/asgi-webdav
 docker image prune -f
 docker container logs -f asgi-webdav
